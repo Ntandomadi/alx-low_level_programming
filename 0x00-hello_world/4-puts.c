4e2f07a1ecc9d94a269498d996c7045d630d745f
Ntandomadi <ntandomadikizela7@gmail.com>
2023-02-16 23:33:32 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzile");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
