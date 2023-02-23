5b180dfdcc08d7de5b4d7f3a7f2c7b6907e84df0
Ntandomadi <ntandomadikizela7@gmail.com>
2023-02-23 06:41:54 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Test funtion for positive or negative
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (o);
+}
