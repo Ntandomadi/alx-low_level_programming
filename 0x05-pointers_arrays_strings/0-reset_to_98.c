1b7978aa166b4123cc56eea5dcc095d168256068
Ntandomadi <ntandomadikizela7@gmail.com>
2023-02-28 02:23:49 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
-  /**
-  * reset_to_98 - reset argument to 98
+
+/**
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
   *
   * Return: void
   */
-
 void reset_to_98(int *n)
 {
 
